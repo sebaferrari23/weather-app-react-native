10cadecc4b21baf31ce57f036e11af26b304438e
Sebastian Ferrari <sebaferrari23@gmail.com>
2021-10-22 12:02:00 -0300

    getting access to user location

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    load()
+  }, [])
+
+  const load = async () => {
+    try {
+      let { status } = await Location.requestBackgroundPermissionsAsync();
+      if(status !== 'granted') {
+        setErrorMessage('Access to location is needed to run the app')
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+      const { latitude, longitude } = location.coords
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+    } catch (e) { console.log(e) }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
